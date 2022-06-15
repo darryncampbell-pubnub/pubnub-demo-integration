52c3978680b03b85c2cf8f287b6c6c5bfec0e718
darryn.campbell <darryn.campbell@pubnub.com>
2022-06-15 20:45:45 +0100

    Updates to work on server-side

--- dist/InteractiveDemoInterface.js
+++ dist/InteractiveDemoInterface.js
@@ -8,8 +8,10 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 /**
  * Indicate to the demo framework that the specified action has been completed
  * @param action The feature to be set
- * @param blockDuplicateCalls Sets a sessionStorage flag so the message is only sent once (saves hammering the keys)
+ * @param blockDuplicateCalls Sets a sessionStorage flag so the message is only sent once (saves hammering the keys).  Only works client-side.
  * @param debug Extra debug info
+ * @param windowLocation When called from the server, you can pass the page URL in so this module can take care of parsing the identifier
+ * @param fetchClient When called from the server, you can pass in an instance of node-fetch which will be used instead of the client fetch APIs
  */
 var actionCompleted = function(args) {
     const pub = 'pub-c-c8d024f7-d239-47c3-9a7b-002f346c1849';
@@ -18,7 +20,35 @@ var actionCompleted = function(args) {
     let action = args.action;
     let blockDuplicateCalls = args.blockDuplicateCalls;
     let debug = args.debug;
-    let queryString = new URL(window.location.href).search.substring(1);
+    let windowLocation = args.windowLocation;
+
+    if (typeof action === 'undefined')
+    {
+        console.log('Interactive Demo Integration Error: No action provided');        
+        return;
+    }
+
+    if (typeof blockDuplicateCalls === 'undefined')
+    {
+        blockDuplicateCalls = true;
+    }
+
+    if (typeof debug === 'undefined')
+    {
+        debug = false;
+    }
+
+    //  If invoked from client-side, you can omit the window location
+    if (typeof windowLocation === 'undefined')
+        windowLocation = window.location.href;
+
+    var fetchClient = null;
+    if (typeof fetch === 'undefined')
+        fetchClient = args.fetchClient;
+    else
+        fetchClient = fetch;
+
+    let queryString = new URL(windowLocation).search.substring(1);
     const urlParamsArray = queryString.split('&');
     for (let i = 0; i < urlParamsArray.length; i++) {
         if (urlParamsArray[i].startsWith('identifier') && urlParamsArray[i].includes('=')) {
@@ -27,29 +57,34 @@ var actionCompleted = function(args) {
         }
     }
     if (identifier === "") {
-        console.log('Guided Demo Integration Error: Failed to detect identifier in URL query string');
+        console.log('Interactive Demo Integration Error: Failed to detect identifier in URL query string');
         return;
     }
     if (blockDuplicateCalls) {
-        //  Read the id from session storage and only send the message if the message was not previous sent
-        let sessionStorageKey = "Demo_" + identifier + action;
-        let storedId = sessionStorage.getItem(sessionStorageKey);
-        if (storedId == null) {
-            if (debug)
-                console.log('Setting session key to avoid duplicate future messages being sent. Action: ' + action + '. Identifier: ' + identifier);
-                sessionStorage.setItem(sessionStorageKey, "set");
-        }
-        else {
-            //  This is a duplicate message, do not send it
-            if (debug)
-                console.log('Message blocked as it is a duplicate. Action: ' + action + '. Identifier: ' + identifier);
-            return;
+        //  This option only works if the sessionStorage object is defined (client-side only)
+        if (!(typeof sessionStorage === 'undefined')) {
+            //  Read the id from session storage and only send the message if the message was not previous sent
+            let sessionStorageKey = "Demo_" + identifier + action;
+            let storedId = sessionStorage.getItem(sessionStorageKey);
+            if (storedId == null) {
+                if (debug)
+                    console.log('Setting session key to avoid duplicate future messages being sent. Action: ' + action + '. Identifier: ' + identifier);
+                    sessionStorage.setItem(sessionStorageKey, "set");
+            }
+            else {
+                //  This is a duplicate message, do not send it
+                if (debug)
+                    console.log('Message blocked as it is a duplicate. Action: ' + action + '. Identifier: ' + identifier);
+                return;
+            }
         }
     }
+
     if (debug)
         console.log('Sending message. Action: ' + action + '. Identifier: ' + identifier);
+    
     const url = `https://ps.pndsn.com/publish/${pub}/${sub}/0/demo/myCallback/${encodeURIComponent(JSON.stringify({ id: identifier, feature: action }))}?store=0&uuid=${identifier}`;
-    fetch(url)
+    fetchClient(url)
         .then(response => {
         if (!response.ok) {
             throw new Error(response.status + ' ' + response.statusText);
@@ -61,7 +96,7 @@ var actionCompleted = function(args) {
         //console.log("Guided Demo Integration success", url, data)
     })
         .catch(e => {
-        console.log('Guided Demo Integration: ', e);
+        console.log('Interactive Demo Integration: ', e);
     });
     return;
 }
